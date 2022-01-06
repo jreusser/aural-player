a701e154a42a59b3ba1dee26bb0d5259c6fcb388
maculateConception <aural.student@gmail.com>
2022-01-07 05:13:31 +0530

    Playing track info

--- iOS/Source/UI/Player/PlayerViewController.swift
+++ iOS/Source/UI/Player/PlayerViewController.swift
@@ -14,17 +14,25 @@ class PlayerViewController: UIViewController {
     @IBOutlet weak var volumeSlider: UISlider!
     @IBOutlet weak var seekSlider: UISlider!
     
+    @IBOutlet weak var imgArt: UIImageView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblArtistAlbum: UILabel!
+    @IBOutlet weak var lblTitleOnly: UILabel!
+    
     let player = objectGraph.playbackDelegate
     var audioGraph = objectGraph.audioGraphDelegate
     
-    private lazy var messenger: Messenger = Messenger(for: self)
+    private lazy var messenger: Messenger = Messenger(for: self, asyncNotificationQueue: .main)
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
         volumeSlider.value = audioGraph.volume
         
-        messenger.subscribe(to: .player_trackTransitioned, handler: trackTransitioned)
+        imgArt.layer.cornerRadius = 4
+        
+        messenger.subscribe(to: .player_trackTransitioned, handler: trackTransitioned(_:))
+        messenger.subscribeAsync(to: .player_trackInfoUpdated, handler: trackInfoUpdated(_:))
     }
     
     @IBAction func playPauseAction(_ sender: Any) {
@@ -65,9 +73,73 @@ class PlayerViewController: UIViewController {
     
     // MARK: Notification handling
     
-    private func trackTransitioned() {
+    private func trackTransitioned(_ notif: TrackTransitionNotification) {
         
         updatePlayPauseButton()
         volumeSlider.value = audioGraph.volume
+        
+        guard let newTrack = notif.endTrack else {
+            
+            [lblTitle, lblArtistAlbum, lblTitleOnly].forEach {$0?.isHidden = true}
+            imgArt.image = nil
+            
+            return
+        }
+        
+        imgArt.image = newTrack.art?.image
+            
+        if let title = newTrack.title {
+            
+            if let artist = newTrack.artist, let album = newTrack.album {
+                
+                // Title, artist, and album
+                lblTitle.text = title
+                lblArtistAlbum.text = "\(artist) -- \(album)"
+                
+                [lblTitle, lblArtistAlbum].forEach {$0?.isHidden = false}
+                lblTitleOnly.isHidden = true
+                
+            } else if let artist = newTrack.artist {
+                
+                // Title and artist
+                lblTitle.text = title
+                lblArtistAlbum.text = artist
+                
+                [lblTitle, lblArtistAlbum].forEach {$0?.isHidden = false}
+                lblTitleOnly.isHidden = true
+                
+            } else if let album = newTrack.album {
+                
+                // Title and album
+                lblTitle.text = title
+                lblArtistAlbum.text = album
+                
+                [lblTitle, lblArtistAlbum].forEach {$0?.isHidden = false}
+                lblTitleOnly.isHidden = true
+                
+            } else {
+                
+                // Title only
+                lblTitleOnly.text = title
+                
+                [lblTitle, lblArtistAlbum].forEach {$0?.isHidden = true}
+                lblTitleOnly.isHidden = false
+            }
+            
+        } else {
+            
+            // Title only
+            lblTitleOnly.text = newTrack.displayName
+            
+            [lblTitle, lblArtistAlbum].forEach {$0?.isHidden = true}
+            lblTitleOnly.isHidden = false
+        }
+    }
+    
+    private func trackInfoUpdated(_ notif: TrackInfoUpdatedNotification) {
+        
+        if notif.updatedTrack == player.playingTrack {
+            imgArt.image = notif.updatedTrack.art?.image
+        }
     }
 }
