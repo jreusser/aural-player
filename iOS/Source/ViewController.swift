b8abbd46891403ad8420bfc796ec4d92a49c8224
maculateConception <aural.student@gmail.com>
2022-01-06 09:39:26 +0530

    Implemented play / pause / previous / next and volume

--- iOS/Source/ViewController.swift
+++ iOS/Source/ViewController.swift
@@ -15,18 +15,32 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var btnSeekForward: UIButton!
     @IBOutlet weak var btnSeekBackward: UIButton!
     
+    @IBOutlet weak var volumeSlider: UISlider!
+    @IBOutlet weak var seekSlider: UISlider!
+    
     let playlist = objectGraph.playlistDelegate
     let player = objectGraph.playbackDelegate
+    var audioGraph = objectGraph.audioGraphDelegate
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        let doubleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap(sender:)))
+        let appTitleView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        appTitleView.image = PlatformImage(named: "AppTitle")!
         
-            doubleTapGestureRecognizer.numberOfTapsRequired = 2
+        navigationItem.titleView = appTitleView
+        
+        volumeSlider.value = audioGraph.volume
+        
+        let doubleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap(sender:)))
+        doubleTapGestureRecognizer.numberOfTapsRequired = 2
         playlistView.addGestureRecognizer(doubleTapGestureRecognizer)
     }
+    
+    // ----------------------------------------------------------------------------------------------------
+    
+    // MARK: Table view
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         playlist.size
@@ -34,15 +48,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PlaylistCell", for: indexPath) as? PlaylistCell else {
-            fatalError("Could not create reusable PlaylistCell !")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlaylistCell", for: indexPath)
+        
+        guard let playlistCell = cell as? PlaylistCell else {
+            return cell
         }
         
         let trackIndex = indexPath.row
-        cell.titleLabel.text = playlist.trackAtIndex(trackIndex)?.displayName ?? "<None>"
-        cell.titleLabel.textColor = .white
+        playlistCell.titleLabel.text = playlist.trackAtIndex(trackIndex)?.displayName ?? "<None>"
+        playlistCell.titleLabel.textColor = .white
         
-        return cell
+        return playlistCell
     }
     
     @objc func handleDoubleTap(sender: UITapGestureRecognizer) {
@@ -54,6 +70,42 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             
             player.play(track)
         }
+    }
+    
+    // ----------------------------------------------------------------------------------------------------
+    
+    // MARK: Actions
+    
+    @IBAction func playPauseAction(_ sender: Any) {
+        
+        player.togglePlayPause()
+        
+        if player.state == .playing {
+            btnPlay.setBackgroundImage(PlatformImage(systemName: "pause"), for: .normal)
+        } else {
+            btnPlay.setBackgroundImage(PlatformImage(systemName: "play"), for: .normal)
+        }
+    }
+    
+    @IBAction func previousTrackAction(_ sender: Any) {
+        player.previousTrack()
+    }
+    
+    @IBAction func nextTrackAction(_ sender: Any) {
+        player.nextTrack()
+    }
+    
+    @IBAction func volumeAction(_ sender: Any) {
+        audioGraph.volume = volumeSlider.value
+    }
+    
+    @IBAction func seekBackwardAction(_ sender: Any) {
+    }
+    
+    @IBAction func seekForwardAction(_ sender: Any) {
+    }
+    
+    @IBAction func seekAction(_ sender: Any) {
     }
 }
 
