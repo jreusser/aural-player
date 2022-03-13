6248c97b7e750c1df114a41def59b56d7dc5dcfc
maculateConception <aural.student@gmail.com>
2022-03-13 11:06:43 +0530

    Fixed main window settings menu icon

--- macOS/Source/UI/CustomViews/Menus/TintedIconMenuItem.swift
+++ macOS/Source/UI/CustomViews/Menus/TintedIconMenuItem.swift
@@ -16,27 +16,9 @@ import Cocoa
 class TintedIconMenuItem: NSMenuItem, ColorSchemeObserver {
     
     // A base image that is used as an image template.
-    @IBInspectable var baseImage: NSImage? {
-        didSet {
-//            self.image = self.baseImage?.filledWithColor(tintFunction())
-        }
-    }
-    
-//    // A function that produces a color used to tint the base image.
-//    var tintFunction: () -> NSColor = {Colors.functionButtonColor} {
-//        didSet {reTint()}
-//    }
-//
-//    // Reapplies the tint (eg. when the tint color has changed or the base image has changed).
-//    func reTint() {
-//        self.image = self.baseImage?.filledWithColor(tintFunction())
-//    }
+    @IBInspectable var baseImage: NSImage?
     
     func colorChanged(to newColor: PlatformColor) {
-        
-        let oldImage = self.image
-        let newImage = self.baseImage!.tintedWithColor(newColor)
-        
-        self.image = self.baseImage?.tintedWithColor(newColor)
+        image = baseImage?.tintedWithColor(newColor)
     }
 }
