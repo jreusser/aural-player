a414119ecae584bea04598f75580a31ff62da711
maculateConception <aural.student@gmail.com>
2022-09-24 17:20:06 +0200

    Added master unit

---
+++ iOS/Source/UI/Effects/Equalizer/EQUnitViewController.swift
@@ -0,0 +1,66 @@
+//
+//  EQUnitViewController.swift
+//  Aural-iOS
+//
+//  Created by Kartik Venugopal on 9/23/22.
+//
+
+import UIKit
+
+class EQUnitViewController: UIViewController {
+    
+    /// The sliders corresponding to all the bands of the equalizer.
+    private var bandSliders: [UISlider] = []
+    
+    @IBOutlet weak var btnBypass: UIButton!
+    
+    private var eqUnit: EQUnitDelegateProtocol = audioGraphDelegate.eqUnit
+    
+    private var rotated: Bool = false
+    
+    ///
+    /// Sets the state of the controls based on the current state of the FX unit.
+    ///
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        navigationItem.title = "Equalizer Settings"
+        
+        if !rotated {
+            
+            // Discover the EQ band sliders among this view's subviews.
+            // The band sliders have a tag value that is >= 0.
+            // Perform filtering to exclude the global gain slider.
+            let allSliders = view.subviews.compactMap {$0 as? UISlider}
+            bandSliders = allSliders.filter {$0.tag >= 0}.sorted(by: {$0.tag < $1.tag})
+            
+            // Rotate the sliders by 90 degrees counter-clockwise (to make them vertical).
+            allSliders.forEach {
+                $0.transform = $0.transform.rotated(by: CGFloat(3 * Float.pi / 2))
+            }
+            
+            rotated = true
+        }
+        
+        btnBypass.tintColor = eqUnit.isActive ? .blue : .gray
+        
+        bandSliders.forEach {
+            $0.value = eqUnit[$0.tag]
+        }
+    }
+    
+    @IBAction func eqBypassAction(_ sender: UIButton) {
+        
+        _ = eqUnit.toggleState()
+        btnBypass.tintColor = eqUnit.isActive ? .blue : .gray
+    }
+    
+    @IBAction func eqGlobalGainAction(_ sender: UISlider) {
+        eqUnit.globalGain = sender.value
+    }
+    
+    @IBAction func eqBandAction(_ sender: UISlider) {
+        eqUnit[sender.tag] = sender.value
+    }
+}
