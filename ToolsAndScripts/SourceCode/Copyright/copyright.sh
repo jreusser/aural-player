61b1289aebfe2dc536a167c353f11eff336d3ea0
Kartik Venugopal <kartikv2017@gmail.com>
2024-02-08 02:28:15 +0100

    Updated copyright to 2024

--- ToolsAndScripts/SourceCode/Copyright/copyright.sh
+++ ToolsAndScripts/SourceCode/Copyright/copyright.sh
@@ -3,7 +3,7 @@
 #  copyright.sh
 #  Aural
 #
-#  Copyright © 2023 Kartik Venugopal. All rights reserved.
+#  Copyright © 2024 Kartik Venugopal. All rights reserved.
 
 export basedir=/Users/kven/Projects/xcode/Aural/Tests
 #export basedir=~/Src
